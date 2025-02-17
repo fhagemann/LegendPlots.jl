1ef4aefa35c03f3cd8433cdc2a02fe15a63e78ef
Felix Hagemann <hagemann@mpp.mpg.de>
2025-02-17 05:01:11 +0100

    Add plot recipe for fits with components

--- ext/Makie/recipes.jl
+++ ext/Makie/recipes.jl
@@ -72,13 +72,90 @@ function LegendPlots.lplot!(
 
     fig = LegendPlots.lplot!(report, legend_position = :none, col = col; kwargs...)
 
-    g = last(Makie.contents(fig[1,col]))
+    g = last(contents(fig[1,col]))
     ax = contents(g)[1]
     lines!(ax, com_report.energy, com_report.values, linewidth = 4, color = :red, linestyle = :dash, label = com_report.label_fit)
     axislegend(ax, position = legend_position)
 
     fig
 end
+
+
+function LegendPlots.lplot!(
+    report::NamedTuple{(:v, :h, :f_fit, :f_components, :gof)};
+    xlabel = "", ylabel = "Counts / bin",
+    title::AbstractString = "", legend_position = :lt,
+    xlims = extrema(first(report.h.edges)), ylims = (0.9,maximum(report.h.weights) * 2),
+    show_label::Bool = true, show_components::Bool = true, yticks = automatic,
+    watermark::Bool = true, show_residuals::Bool = true, col::Int = 1, kwargs...
+)
+
+    fig = current_figure()
+
+    # create plot
+    g = GridLayout(fig[1,col])
+    ax = Axis(g[1,1], 
+        title = title, titlefont = :bold, titlesize = 16pt, 
+        xlabel = xlabel, ylabel = ylabel, yticks = yticks,
+        limits = (xlims, ylims),
+        yscale = log10
+    )
+
+    hist!(ax, midpoints(first(report.h.edges)), weights = report.h.weights, bins = first(report.h.edges), color = LegendPlots.DiamondGrey, label = "Data")
+    lines!(range(xlims..., length = 1000), x -> report.f_fit(x) * step(first(report.h.edges)), color = :black, label = ifelse(show_label, "Best Fit" * (!isempty(report.gof) ? " (p = $(round(report.gof.pvalue, digits=2)))" : ""), ""))
+
+                
+    if show_components
+        for (idx, component) in enumerate(keys(report.f_components.funcs))
+            lines!(
+                range(extrema(first(report.h.edges))..., length = 1000), 
+                x -> report.f_components.funcs[component](x) * step(first(report.h.edges)), 
+                color = report.f_components.colors[component], 
+                label = ifelse(show_label, report.f_components.labels[component], ""),
+                linestyle = report.f_components.linestyles[component],
+                linewidth = 4
+            )
+        end
+    end
+
+    if legend_position != :none 
+        axislegend(ax, position = legend_position)
+    end
+
+    if !isempty(report.gof) && show_residuals
+
+        ax.xticklabelsize = 0
+        ax.xticksize = 0
+        ax.xlabel = ""
+
+        ax2 = Axis(g[2,1],
+                xlabel = xlabel, ylabel = "Residuals (σ)",
+                yticks = -3:3:3, limits = (xlims,(-5,5))
+            )
+        residualplot!(ax2, (x = midpoints(first(report.h.edges)), residuals_norm = report.gof.residuals_norm))
+
+        linkxaxes!(ax, ax2)
+        rowgap!(g, 0)
+        rowsize!(g, 1, Auto(3))
+
+        # align ylabels
+        yspace = maximum(tight_yticklabel_spacing!, (ax, ax2))
+        ax.yticklabelspace = yspace
+        ax2.yticklabelspace = yspace
+    end
+
+    all = Axis(g[:,:])
+    hidedecorations!(all)
+    hidespines!(all)
+    current_axis!(all)
+
+    # add watermarks
+    watermark && LegendPlots.add_watermarks!(; kwargs...)
+
+    fig
+end
+
+
 
 function LegendPlots.lplot!(
         chinfo::Table, pars::PropDict, properties::AbstractVector{Symbol};
@@ -164,7 +241,7 @@ function LegendPlots.lhist!(
         )
     end
 
-    hm = Makie.heatmap!(ax, h.edges..., replace(h.weights, 0 => NaN), colormap = :magma, colorscale = log10)
+    hm = heatmap!(ax, h.edges..., replace(h.weights, 0 => NaN), colormap = :magma, colorscale = log10)
     hm.rasterize = rasterize
     cb = Colorbar(fig[1,2], hm, 
         tickformat = values -> rich.("10", superscript.(string.(Int.(log10.(values))))),
